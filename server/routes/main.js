5b6b0ccb352b13c05093e940488323686c44bc0d
nihalgunu <nihalgunukula@gmail.com>
2024-07-22 22:48:49 -0700

    made card for profile card

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,25 +5,40 @@ router.get('', (req, res) => {
     const locals = {
         title: "Nihal Gunukula",
         description: "Learning more about me."
-    }
-
-    res.render('index', { locals });
+    };
+    res.render('index', { locals, showProfileCard: false });
 });
 
 router.get('/about', (req, res) => {
-    res.render('about');
+    const locals = {
+        title: "About Me",
+        description: "About Nihal Gunukula."
+    };
+    res.render('about', { locals, showProfileCard: true });
 });
 
 router.get('/applications', (req, res) => {
-    res.render('applications');
+    const locals = {
+        title: "Applications",
+        description: "Nihal Gunukula's Applications."
+    };
+    res.render('applications', { locals, showProfileCard: true });
 });
 
 router.get('/research', (req, res) => {
-    res.render('research');
+    const locals = {
+        title: "Research",
+        description: "Nihal Gunukula's Research."
+    };
+    res.render('research', { locals, showProfileCard: true });
 });
 
 router.get('/resume', (req, res) => {
-    res.render('resume');
+    const locals = {
+        title: "Resume",
+        description: "Nihal Gunukula's Resume."
+    };
+    res.render('resume', { locals, showProfileCard: false });
 });
 
 module.exports = router;
